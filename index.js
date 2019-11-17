7e88e2b4cdf83c339f4f66b7a1617636a5063c13
Nabil Ridhwanshah <imnabilridhwan@gmail.com>
2019-11-17 12:19:16 +0800

    Testing stage...

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 let user_details = null;
+let response_json = null;
 
 function authorise_user(client_id, redirect_uri, show_dialog, scope) {
     let redirect_link = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&redirect_uri=${redirect_uri}&show_dialog=${show_dialog}&scope=${scope}`
@@ -7,30 +8,59 @@ function authorise_user(client_id, redirect_uri, show_dialog, scope) {
 }
 
 //  Check if authenticated
-if(window.location.hash){
+if (window.location.hash) {
     let dt = new Date()
     user_details = {
         "access_token": window.location.hash.substr(1).split("&")[0].split("=")[1],
         "type": access_token = window.location.hash.substr(1).split("&")[1].split("=")[1].toLowerCase(),
         "expires_in": dt.setHours(dt.getHours() + 1),
     }
+} else {
+    document.getElementById("body").innerHTML = `
+<a href="${authorise_user("e172f1be70f64206a8b79f5e8a1f03df", "https://nabilridhwan.github.io/spotifyer", true, "user-library-read")}">Authenticate</a>
+`
 }
 
-// Check if authenticated
-if(user_details){
-    fetch("https://api.spotify.com/v1/me/tracks", {
+// Check if object is already initiated
+if (user_details) {
+    fetch("https://api.spotify.com/v1/me/tracks?limit=50", {
         method: "GET",
         headers: {
-            "Authorisation": `Bearer ${user_details.access_token}`
+            "Authorization": `Bearer ${user_details.access_token}`
         }
     }).then(response => {
-        console.log(response)
-        return response.json()
+        if (response.status == 200) {
+            return response.json()
+        } else {
+            throw new Error('Something went wrong fetching the API!')
+        }
     }).then(json => {
-        console.log(json)
+        response_json = json
+        display()
     })
 }
 
-document.getElementById("body").innerHTML = `
-<a href="${authorise_user("e172f1be70f64206a8b79f5e8a1f03df", "http://127.0.0.1:8080/", true, "user-library-read")}">Authenticate</a>
-`
+function display(){
+    // console.log(response_json)
+
+    response_json.items.forEach(item => {
+        let track_name = item.track.name
+        let track_artist_name = item.track.artists[0].name
+        let album_image = item.track.album.images[0].url
+        let open_html_link = item.track.external_urls.spotify;
+
+        // console.log(item.track.external_urls.spotify)
+
+        document.getElementById("main_body").innerHTML += `
+        <div class="track">
+            <a href="${open_html_link}">
+                <img src=${album_image} />
+            </a>
+        </div>
+        `
+
+        // item.track.artists.forEach(artist => {
+        //     console.log("By: " + artist.name)
+        // })
+    })
+}
