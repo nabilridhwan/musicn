b705261f2faa8c9f7e0ef42ba86d70584014f296
Nabil Ridhwan <nabridhwan@gmail.com>
2022-03-05 00:29:20 +0800

    Added share button

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { FaUser, FaSpotify } from "react-icons/fa"
+import { FaUser, FaSpotify, FaShare, FaShareAlt } from "react-icons/fa"
 import NavigationBar from "../components/NavigationBar";
 
 export default function User() {
@@ -10,6 +10,8 @@ export default function User() {
     const [user, setUser] = useState({})
     const [currentSong, setCurrentSong] = useState(null)
     const [topSongs, setTopSongs] = useState(null)
+    const [shareButtonText, setShareButtonText] = useState("Share with your friends!")
+                
 
 
     useEffect(() => {
@@ -54,6 +56,20 @@ export default function User() {
             .then(topSongs => {
                 return topSongs.items
             })
+    }
+
+    async function handleShare(){
+        try{
+
+        await navigator.share({
+            url: window.location.href,
+            title: "Musicn",
+            text: "Check out my top songs of the month!"
+        })
+
+        }catch(e){
+            console.log(e)
+        }
     }
 
     return (
@@ -166,6 +182,14 @@ export default function User() {
             ) : (
                 <p className="text-center italic">Wow this is scary! This user does not have their top songs!</p>
             )}
+
+            <div className="fixed bottom-0 w-full flex justify-center items-center">
+                <button onClick={handleShare} className="bg-blue-500 transition ease-out duration-500 rounded-lg p-3 mb-5 flex items-center btn-anim shadow-lg shadow-blue-500/50 text-white">
+
+<FaShareAlt className="mr-2" />
+{shareButtonText}
+                </button>
+            </div>
         </div>
     )
 }
