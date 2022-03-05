d6c28f0023dd3d4f90bb0c981bef88dd65f65b93
Nabil Ridhwan <nabridhwan@gmail.com>
2022-03-05 16:13:56 +0800

    fix(Profile Page): :children_crossing: Profile page now has conditional rendering
    Profile page will fully render when all APIs are fetched

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -18,6 +18,7 @@ export default function Profile() {
     let navigate = useNavigate();
 
     const [username, setUsername] = useState("");
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         if (document.cookie.length == 0) {
@@ -27,6 +28,7 @@ export default function Profile() {
             let profile = await getUserProfile();
             setUser(profile[0])
             setUsername(profile[0].username)
+            setLoaded(true)
         })();
 
     }, [])
@@ -91,85 +93,95 @@ export default function Profile() {
     return (
         <div>
 
+
+
             <NavigationBar />
 
-            <div className="my-10 flex flex-col items-center">
+            {loaded && (
+                <>
+                    <div className="my-10 flex flex-col items-center">
 
-                <img src={user.profile_pic_url} className="profile_picture rounded-full w-24 h-24" />
+                        <img src={user.profile_pic_url} className="profile_picture rounded-full w-24 h-24" />
 
-                <h2 className="text-3xl font-bold">{user && user.name}</h2>
-                <p className="text-sm text-black/50" id="follower-count-text">
-                    @{user.username}
-                </p>
+                        <h2 className="text-3xl font-bold">{user && user.name}</h2>
+                        <p className="text-sm text-black/50" id="follower-count-text">
+                            @{user.username}
+                        </p>
 
-                <Link to={"/user/" + user.username} id="spotify-profile-link"
-                    className="flex mx-auto mt-6 justify-center items-center px-3 py-2 bg-blue-500 text-white rounded-lg hover:shadow-md hover:shadow-blue-500 transition ease-out duration-500">
-                    Go to profile page
-                </Link>
+                        <Link to={"/user/" + user.username} id="spotify-profile-link"
+                            className="flex mx-auto mt-6 justify-center items-center px-3 py-2 bg-blue-500 text-white rounded-lg hover:shadow-md hover:shadow-blue-500 transition ease-out duration-500">
+                            Go to profile page
+                        </Link>
 
-                <a href={reauth_url} id="spotify-profile-link"
-                    className="flex mx-auto mt-6 justify-center items-center px-3 py-2 bg-spotify-green text-white rounded-lg hover:shadow-md hover:shadow-spotify-green/50 transition ease-out duration-500">
-                    Reauthenticate Spotify Account (If you have errors!)
-                </a>
 
-                {!user.refresh_token && (
+                        {!user.refresh_token ? (
 
-                    <div className="mt-6 flex-col md:flex-row bg-white border border-black/20 drop-shadow-lg p-10 rounded-2xl w-2/3">
-                        <FaExclamationTriangle className="text-red-500 text-4xl" />
+                            <div className="mt-6 flex-col md:flex-row bg-white border border-black/20 drop-shadow-lg p-10 rounded-2xl w-2/3">
+                                <FaExclamationTriangle className="text-red-500 text-4xl" />
 
-                        <div className="mt-2">
+                                <div className="mt-2">
 
-                            <p className="font-bold text-2xl text-left">
-                                Link your Spotify Account
-                            </p>
+                                    <p className="font-bold text-2xl text-left">
+                                        Link your Spotify Account
+                                    </p>
 
-                            <p className="text-left text-black/50">
-                                Your friends won’t be able to see your Spotify statistics unless you link your Spotify account!
-                            </p>
+                                    <p className="text-left text-black/50">
+                                        Your friends won’t be able to see your Spotify statistics unless you link your Spotify account!
+                                    </p>
 
-                            <a id="spotify-profile-link"
-                                href="http://localhost:4000/api/auth"
-                                className="flex mt-5 justify-center items-center px-3 py-2 bg-spotify-green text-white rounded-lg hover:shadow-md hover:shadow-spotify-green/50 transition ease-out duration-500">
-                                <FaSpotify className="fa fa-spotify text-1xl text-center text-white mr-2" aria-hidden="true"></FaSpotify>
-                                Link your Spotify
+                                    <a id="spotify-profile-link"
+                                        href={reauth_url}
+                                        className="flex mt-5 justify-center items-center px-3 py-2 bg-spotify-green text-white rounded-lg hover:shadow-md hover:shadow-spotify-green/50 transition ease-out duration-500">
+                                        <FaSpotify className="fa fa-spotify text-1xl text-center text-white mr-2" aria-hidden="true"></FaSpotify>
+                                        Link your Spotify
+                                    </a>
+                                </div>
+
+                            </div>
+                        ) : (
+                            <a href={reauth_url} id="spotify-profile-link"
+                                className="flex mx-auto mt-6 justify-center items-center px-3 py-2 bg-spotify-green text-white rounded-lg hover:shadow-md hover:shadow-spotify-green/50 transition ease-out duration-500">
+                                Reauthenticate Spotify Account (If you have errors!)
                             </a>
-                        </div>
+                        )}
+
+
 
                     </div>
-                )}
+
+                    <div className="container">
 
 
+                        <p className="text-red-500 text-center">
+                            {error}
+                        </p>
 
-            </div>
+                        <h1 className="text-center text-2xl font-bold">
+                            Edit Profile
+                        </h1>
 
-            <div className="container">
+                        <label htmlFor="username">
+                            Username
+                        </label>
 
+                        <input type="text" required id="username" placeholder="Username" value={username} className="block w-full" onChange={e => setUsername(e.target.value)} />
 
-                <p className="text-red-500 text-center">
-                    {error}
-                </p>
+                        {username != user.username && <button className="border rounded-lg w-full text-center bg-brand-color py-3" onClick={() => {
+                            handleChangeUsername()
+                        }}>
+                            Save Username</button>}
 
-                <h1 className="text-center text-2xl font-bold">
-                    Edit Profile
-                </h1>
+                        <button onClick={handleLogout} id="spotify-profile-link"
+                            href={"https://open.spotify.com/user/" + user.spotify_userid}
+                            className="flex mx-auto mt-6 justify-center items-center px-3 py-2 bg-red-500 text-white rounded-lg hover:shadow-md hover:shadow-red-500 transition ease-out duration-500">
+                            Log Out
+                        </button>
+                    </div>
 
-                <label htmlFor="username">
-                    Username
-                </label>
+                </>
 
-                <input type="text" required id="username" placeholder="Username" value={username} className="block w-full" onChange={e => setUsername(e.target.value)} />
+            )}
 
-                {username != user.username && <button className="border rounded-lg w-full text-center bg-brand-color py-3" onClick={() => {
-                    handleChangeUsername()
-                }}>
-                    Save Username</button>}
-
-                <button onClick={handleLogout} id="spotify-profile-link"
-                    href={"https://open.spotify.com/user/" + user.spotify_userid}
-                    className="flex mx-auto mt-6 justify-center items-center px-3 py-2 bg-red-500 text-white rounded-lg hover:shadow-md hover:shadow-red-500 transition ease-out duration-500">
-                    Log Out
-                </button>
-            </div>
 
 
         </div>
