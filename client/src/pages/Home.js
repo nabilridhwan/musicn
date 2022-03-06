cb77f4c956d61aed3537923ff8135414e3277ef3
Nabil Ridhwan <nabridhwan@gmail.com>
2022-03-06 21:45:56 +0800

    fix: :lipstick: Updated UI to match brand color and NavBar changes
    Button now have brand color and NavBar only contains profile picture if logged in

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,21 +8,21 @@ export default function Home() {
             <NavigationBar />
 
             <div className="jumbotron my-24">
-                <h1>
+                <h1 className="my-10">
                     Share your top songs with your friends!
                 </h1>
 
-                <h3 className="my-4 text-lg">
+                <p className="my-4 text-lg leading-relaxed">
                     With <span className="font-bold">Musicn</span>, you have your own Spotify profile page which shows the song you're listening to along with your top songs of the month! Copy the link and send it to your friends (or maybe, paste it in your Instagram's bio!)
-                </h3>
+                </p>
 
-                <a href="/signup" className="block bg-spotify-green shadow-lg shadow-spotify-green/50 w-fit m-auto p-4 rounded-lg my-20 text-white font-bold">
+                <a href="/signup" className="block bg-brand-color shadow-lg shadow-spotify-green/50 w-fit m-auto p-4 rounded-lg my-1 text-brand-text-color font-bold">
                     Get Started
                 </a>
 
 
 
-                <p className="my-5">
+                <p className="my-20">
                     This little web application is made by <a href="https://github.com/nabilridhwan" className="underline font-bold">Nabil Ridhwan</a>.
                     And is made possible using Node.js, Express, Tailwind CSS, Supabase, React and Spotify's API. <a href="https://github.com/nabilridhwan/spotifyer" className="underline font-bold">Musicn</a> is open-sourced and will always be!
 
